520273542a18954e4fc2c8ffc5a9e041a5fee9e2
Gaysin.R <xpan96@gmail.com>
2018-09-03 10:33:31 +0300

    add fragment for menu item

--- app/src/main/java/ru/gkkdev/x/dynamicimage/MainActivity.java
+++ app/src/main/java/ru/gkkdev/x/dynamicimage/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.gkkdev.x.dynamicimage;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,8 +14,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import ru.gkkdev.x.dynamicimage.fragments.first_fragment;
+import ru.gkkdev.x.dynamicimage.fragments.second_fragment;
+import ru.gkkdev.x.dynamicimage.fragments.settings_fragment;
+import ru.gkkdev.x.dynamicimage.fragments.three_fragment;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    first_fragment f_fragment;
+    second_fragment s_fragment;
+    three_fragment t_fragment;
+    settings_fragment setting_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        f_fragment = new first_fragment();
+        s_fragment = new second_fragment();
+        t_fragment = new three_fragment();
+        setting_fragment = new settings_fragment();
+
     }
 
     @Override
@@ -80,15 +97,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        if (id == R.id.first_fragment) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        }
+            setTitle("10 sec fragment");
+            fragmentTransaction.replace(R.id.container, f_fragment);
+        } else if (id == R.id.second_fragment) {
+            setTitle("60 sec fragment");
+            fragmentTransaction.replace(R.id.container, s_fragment);
+        } else if (id == R.id.three_fragment) {
+            setTitle("24 hour fragment");
+            fragmentTransaction.replace(R.id.container, t_fragment);
+        } else if (id == R.id.fo_fragment) {
+            setTitle("Settings");
+            fragmentTransaction.replace(R.id.container, setting_fragment);
+        } fragmentTransaction.commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
